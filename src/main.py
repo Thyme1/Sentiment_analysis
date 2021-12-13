188d09ee1f09d8cd79ab51ff17d9038feae99a40
Thyme1 <konrad1jasinski@gmail.com>
2021-12-13 22:47:37 +0100

    extract ratings

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-page_url = 'https://lubimyczytac.pl/ksiazki/opinie'
-page = requests.get(page_url)
 
-soup = BeautifulSoup(page.content, 'html.parser')
 
 # print(soup.prettify())  # ładniejszy sposób na wyświetlenie
 
 # div class="comment-cloud__body relative"
 # <p class="p-expanded js-expanded mb-0" style>
 # mydivs = soup.find_all("div", {"class": "comment-cloud__body relative"})
-my_paragraphs = soup.find_all("p", class_="p-expanded js-expanded mb-0")
-my_ratings = soup.find_all("span", class_="big-number")
+for i in range(3):
+    page_url = 'https://lubimyczytac.pl/ksiazki/opinie/' + str(i)
+    page = requests.get(page_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-print(my_ratings)
+    # my_paragraphs = soup.find_all("p", class_="p-expanded js-expanded mb-0")
+    my_ratings = soup.find_all("span", class_="big-number")
+    for rating in my_ratings:
+        rat = re.search(r'\d' , str(rating))
+        print(rat.group())
+
+
+
+
+    # with open("C:\\Users\\konra\\PycharmProjects\\Sentiment_analysis\\scraped.txt", "a") as fp:
+    #     print([x.decompose() for x in soup.find_all("span", class_="big-number")])
+
+
+
+
+
 
 
 
