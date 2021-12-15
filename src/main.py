51e2557c6ab6273d8d7ae1e4c8f9bf49de895e44
Thyme1 <konrad1jasinski@gmail.com>
2021-12-15 15:47:05 +0100

    create scraper rating and desc in one line

--- src/main.py
+++ src/main.py
@@ -3,29 +3,23 @@ from bs4 import BeautifulSoup
 import re
 
 
-
-# print(soup.prettify())  # ładniejszy sposób na wyświetlenie
-
-# div class="comment-cloud__body relative"
-# <p class="p-expanded js-expanded mb-0" style>
-# mydivs = soup.find_all("div", {"class": "comment-cloud__body relative"})
 for i in range(3):
     page_url = 'https://lubimyczytac.pl/ksiazki/opinie/' + str(i)
     page = requests.get(page_url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    # my_paragraphs = soup.find_all("p", class_="p-expanded js-expanded mb-0")
     my_ratings = soup.find_all("span", class_="big-number")
-    for rating in my_ratings:
-        rat = re.search(r'\d' , str(rating))
-        print(rat.group())
+    my_paragraphs = soup.find_all("p", class_="p-expanded js-expanded mb-0")
+    my_paragraph_str = str(my_paragraphs)[1:-1]
+    my_paragraph_str = my_paragraph_str.replace("\n", " ")
+    my_paragraph_str = my_paragraph_str.replace("<br/>", "")
+    my_paragraph_str = my_paragraph_str.replace("<br>", "")
+    paragraph = re.findall(r'(?<=<p class=\"p-expanded js-expanded mb-0\" style=\"display:none;\">)(.*?)(?=</p>)',my_paragraph_str)
 
-
-
-
-    # with open("C:\\Users\\konra\\PycharmProjects\\Sentiment_analysis\\scraped.txt", "a") as fp:
-    #     print([x.decompose() for x in soup.find_all("span", class_="big-number")])
-
+    my_ratings = soup.find_all("span", class_="big-number")
+    for rating,item in zip(my_ratings,paragraph):
+        rat = re.search(r'\d+', str(rating))
+        print(rat.group(),item)
 
 
 
