b829cf8455be44dd4738e60c65ea486d0f395cb7
Thyme1 <konrad1jasinski@gmail.com>
2021-12-15 16:15:14 +0100

    problem with opinions without rating

--- src/main.py
+++ src/main.py
@@ -1,25 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import sys
 
 
-for i in range(3):
-    page_url = 'https://lubimyczytac.pl/ksiazki/opinie/' + str(i)
-    page = requests.get(page_url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+with open("C:\\Users\\konra\\PycharmProjects\\Sentiment_analysis\\scraped.txt", "a",encoding="utf-8") as fp:
+    for i in range(10):
+        page_url = 'https://lubimyczytac.pl/ksiazki/opinie/' + str(i)
+        page = requests.get(page_url)
+        soup = BeautifulSoup(page.content, 'html.parser')
 
-    my_ratings = soup.find_all("span", class_="big-number")
-    my_paragraphs = soup.find_all("p", class_="p-expanded js-expanded mb-0")
-    my_paragraph_str = str(my_paragraphs)[1:-1]
-    my_paragraph_str = my_paragraph_str.replace("\n", " ")
-    my_paragraph_str = my_paragraph_str.replace("<br/>", "")
-    my_paragraph_str = my_paragraph_str.replace("<br>", "")
-    paragraph = re.findall(r'(?<=<p class=\"p-expanded js-expanded mb-0\" style=\"display:none;\">)(.*?)(?=</p>)',my_paragraph_str)
+        my_ratings = soup.find_all("span", class_="big-number")
+        my_paragraphs = soup.find_all("p", class_="p-expanded js-expanded mb-0")
+        my_paragraph_str = str(my_paragraphs)[1:-1]
+        my_paragraph_str = my_paragraph_str.replace("\n", " ")
+        my_paragraph_str = my_paragraph_str.replace("<br/>", "")
+        my_paragraph_str = my_paragraph_str.replace("<br>", "")
+        my_paragraph_str = re.sub(r'</br>', '', my_paragraph_str)
+        paragraph = re.findall(r'(?<=<p class=\"p-expanded js-expanded mb-0\" style=\"display:none;\"> )(.*?)(?=</p>)',my_paragraph_str)
 
-    my_ratings = soup.find_all("span", class_="big-number")
-    for rating,item in zip(my_ratings,paragraph):
-        rat = re.search(r'\d+', str(rating))
-        print(rat.group(),item)
+
+
+        my_ratings = soup.find_all("span", class_="big-number")
+        for rating,item in zip(my_ratings,paragraph):
+            rat = re.search(r'\d+', str(rating))
+            sys.stdout = fp
+            print(rat.group(),';',item,sep='')
 
 
 
