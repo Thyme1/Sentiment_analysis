cda25533f8fc1aed731b9ee03652b4c42244f234
Thyme1 <konrad1jasinski@gmail.com>
2021-12-22 22:17:14 +0100

    1.65 rmsa

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import re
+import sys
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from vowpalwabbit import pyvw
@@ -14,13 +15,13 @@ def change_into_training_data(opinion, test):
         train_line += opinion[0] + ' |' + 'characters:.' + str(len(opinion[1]))
     word_list = opinion[1].split()
     train_line += ' words:.' + str(len(word_list))
-    exclamation_marks = opinion[1].count('!')
-    train_line += ' exclamation_marks:.' + str(exclamation_marks)
+    train_line += ' exclamation_marks:.' + str(opinion[1].count('!'))
+    train_line += ' question_marks:.' + str(opinion[1].count('?'))
     positive_words_count = sum(el.lower() in POSITIVE for el in word_list)
     negative_words_count = sum(el.lower() in NEGATIVE for el in word_list)
     train_line += ' positive_words:.' + str(positive_words_count)
     train_line += ' negative_words:.' + str(negative_words_count)
-    result = re.sub(r'[0-9]+|:', ' ', opinion[1])
+    result = re.sub(r'[0-9]+|:|\.|\?|!|;|"', ' ', opinion[1])
     train_line += ' plain_text:.' + str(result.lower())
 
     return train_line
@@ -30,50 +31,60 @@ model = pyvw.vw(quiet=False)
 
 POSITIVE = ['akcja', 'aktywna', 'aktywnie', 'aktywny', 'altruistyczny', 'ambitne', 'ambitnie', 'ambitny', 'anielska',
             'anielski', 'anielskie', 'anielsko', 'atrakcyjna', 'atrakcyjne', 'atrakcyjnie', 'atrakcyjny', 'autentyczna',
-            'autentycznie', 'autentyczny', 'bezpośredni', 'bezpośrednia', 'bezpośrednie', 'bezpośrednio', 'bezstronna',
-            'bezstronnie', 'bezstronny', 'bliska', 'bliski', 'bliskie', 'blisko', 'bliskość', 'bogactwo', 'bogata',
-            'bogaty', 'bogini', 'boska', 'boski', 'bratnia dusza', 'brawo', 'buziaki', 'bystra', 'bystre', 'bystry',
-            'błoga', 'błogi', 'błogo', 'błogosławieństwa', 'błogosławieństwo', 'błogosławiony', 'błogość', 'błyszczy',
-            'cały', 'całym sercem', 'ceniona', 'ceniony', 'cenna', 'cenny', 'charyzma', 'charyzmatyczna',
-            'charyzmatyczny', 'chętna', 'chętnie', 'chętny', 'chęćciepły', 'ciekawe', 'ciekawie', 'ciepła', 'ciepło',
-            'cierpliwość', 'cierpliwy', 'cieszyć', 'cieszę', 'cnotliwy', 'cud', 'cudowny', 'czarujący', 'czule',
-            'czuć się dobrze', 'czuła', 'czułość', 'czuły', 'czysta miłość i światło', 'czysty', 'delikatna',
-            'delikatnie', 'delikatny', 'dobra', 'dobre', 'dobrze', 'empatyczna', 'empatyczny', 'fantastyczna',
-            'fantastyczne', 'fantastycznie', 'fantastyczny', 'fantazja', 'fantazje', 'geniusz', 'godny podziwu',
-            'gorliwa', 'gorliwie', 'gorliwy', 'gratulacje', 'gratuluję', 'harmonia', 'harmonijnie', 'harmonijny',
-            'hojna', 'hojne', 'hojnie', 'hojny', 'humor', 'humorystycznie', 'humorystyczny', 'huumorystyczna',
-            'idealna', 'idealnie', 'ideał', 'imponuje', 'imponująca', 'imponujące', 'imponujący', 'innowacja',
-            'innowacyjna', 'innowacyjnie', 'innowacyjny', 'inspiracja', 'inspirować', 'inspirująca', 'inspirujące',
-            'instynktownie', 'intelegtualny', 'intelekt', 'intelektualna', 'inteligencja', 'inteligentna',
-            'inteligentny', 'intuicja', 'intuicyjna', 'intuicyjny', 'istotna', 'istotnie', 'istotny', 'jasna', 'jasne',
-            'kochający', 'kocham', 'kochana', 'kochałam', 'kochałem', 'komfort', 'komfortowa', 'komfortowe',
-            'korzystna', 'korzystne', 'korzystnie', 'kreatywna', 'kreatywne', 'kreatywnie', 'kreatywny', 'kwitnący',
-            'lekka', 'lekki', 'lekko', 'mistrz', 'mistrzostwo', 'mistrzowski', 'mistrzyni', 'mądra', 'mądre', 'mądrość',
-            'mądry', 'mądrze', 'młodzieńczy', 'najlepiej', 'najlepsza', 'najlepsze', 'najlepszy', 'niebybywały',
-            'niebywałacałuski', 'niebywałe', 'niezapomniana', 'niezapomniana', 'niezapomniany', 'niezapomniany',
-            'niezawodna', 'niezawodnie', 'niezawodny', 'niezrównana', 'niezrównany', 'niezwykla', 'niezwykły',
-            'niezywkła', 'oczarowana', 'oczarowany', 'oryginalnie', 'oryginalność', 'oryginalny', 'oszałamiająca',
-            'oszałamiające', 'oszałamiający', 'pełen', 'pełna', 'podekscytowana', 'podekscytowanie', 'podekscytowany',
-            'pokochałam', 'pokochałem', 'pomysłowa', 'pomysłowy', 'pozytwna', 'pozytywnie', 'przygoda', 'przyjemna',
-            'przyjemne', 'przyjemnie', 'przyjemny', 'radosna', 'radosne', 'radosne', 'radośnie', 'rozkosz', 'rozkoszna',
-            'rozkosznie', 'rozkoszny', 'silna', 'silnie', 'silny', 'sukces', 'szczera', 'szczerość', 'szczery',
-            'szczerze', 'szlachetna', 'szlachetnie', 'szlachetny', 'sztuka', 'twórcza', 'twórcze', 'twórczy', 'udana',
-            'udanie', 'ukochana', 'ukochany', 'ulubiona', 'ulubione', 'urok', 'uwielbiam', 'wciągająca', 'wciągnął',
-            'wciągnęła', 'wow', 'wyobraźnia', 'wyobraźnię', 'zabawa', 'zabawna', 'zabawnie', 'zachwycona', 'zachwycony',
-            'zachwyt', 'zywa', 'ładna', 'ładnie', 'ładny', 'łał', 'świadoma', 'świadomie', 'świadomy', 'świetna',
-            'świetne', "😉", "każdy", 'świetnie', "esencja", "emanowała", "rzetelna", "rzetelnie", "rzetelność",
-            "czekam", 'świetnyciekawa', 'żywo', 'żywy', "świetna", "świetny", "świetnie", "polecam", "gorąco",
-            "doskonała", "doskonale", "doskonały", "porusza", "poruszyła", 'poruszająca', "zaciekawiła",
-            "zaintrygowała", "zafascynowała", "zafascynowanie", "zaciekawienie", "zaciekawiony", "zaciekawiona",
-            "interesująca", "bardzo", "fajna", "fajny", "fajnie", "❤"]
+            'autentycznie', 'autentyczny', 'bardzo', 'bawić', 'bezpośredni', 'bezpośrednia', 'bezpośrednie',
+            'bezpośrednio', 'bezstronna', 'bezstronnie', 'bezstronny', 'bliska', 'bliski', 'bliskie', 'blisko',
+            'bliskość', 'bogactwo', 'bogata', 'bogaty', 'bogini', 'bomba', 'boska', 'boski', 'bratnia dusza', 'brawo',
+            'brawo', 'buziaki', 'bystra', 'bystre', 'bystry', 'błoga', 'błogi', 'błogo', 'błogosławieństwa',
+            'błogosławieństwo', 'błogosławiony', 'błogość', 'błyszczy', 'cały', 'całym sercem', 'ceniona', 'ceniony',
+            'cenna', 'cenny', 'charyzma', 'charyzmatyczna', 'charyzmatyczny', 'chętna', 'chętnie', 'chętny',
+            'chęćciepły', 'ciekawe', 'ciekawie', 'ciepła', 'ciepło', 'cierpliwość', 'cierpliwy', 'cieszyć', 'cieszę',
+            'cnotliwy', 'cud', 'cudowny', 'czarujący', 'czekam', 'czule', 'czuć się dobrze', 'czuła', 'czułość',
+            'czuły', 'czysta miłość i światło', 'czysty', 'delikatna', 'delikatnie', 'delikatny', 'dobra', 'dobre',
+            'dobrze', 'doskonale', 'doskonała', 'doskonały', 'dowcipna', 'emanowała', 'empatyczna', 'empatyczny',
+            'esencja', 'fajna', 'fajnie', 'fajny', 'fantastyczna', 'fantastyczne', 'fantastycznie', 'fantastyczny',
+            'fantazja', 'fantazje', 'geniusz', 'godny podziwu', 'gorliwa', 'gorliwie', 'gorliwy', 'gorąco',
+            'gratulacje', 'gratuluję', 'harmonia', 'harmonijnie', 'harmonijny', 'hojna', 'hojne', 'hojnie', 'hojny',
+            'homor', 'humor', 'humor', 'humorystycznie', 'humorystyczny', 'huumorystyczna', 'idealna', 'idealna',
+            'idealnie', 'ideał', 'imponuje', 'imponująca', 'imponujące', 'imponujący', 'innowacja', 'innowacyjna',
+            'innowacyjnie', 'innowacyjny', 'inspiracja', 'inspirować', 'inspirująca', 'inspirujące', 'instynktownie',
+            'intelegtualny', 'intelekt', 'intelektualna', 'inteligencja', 'inteligentna', 'inteligentny',
+            'interesująca', 'intuicja', 'intuicyjna', 'intuicyjny', 'istotna', 'istotnie', 'istotny', 'jasna', 'jasne',
+            'każdy', 'klimat', 'klimatem', 'klimatu', 'kochający', 'kocham', 'kochana', 'kochałam', 'kochałem',
+            'komfort', 'komfortowa', 'komfortowe', 'korzystna', 'korzystne', 'korzystnie', 'kreatywna', 'kreatywne',
+            'kreatywnie', 'kreatywny', 'kwitnący', 'lekka', 'lekki', 'lekko', 'mega', 'mistrz', 'mistrzostwo',
+            'mistrzowski', 'mistrzyni', 'mądra', 'mądre', 'mądrość', 'mądry', 'mądrze', 'młodzieńczy', 'najlepiej',
+            'najlepsza', 'najlepsze', 'najlepszy', 'napięcie', 'napięciu', 'niebybywały', 'niebywałacałuski',
+            'niebywałe', 'niezapomniana', 'niezapomniana', 'niezapomniany', 'niezapomniany', 'niezawodna',
+            'niezawodnie', 'niezawodny', 'niezrównana', 'niezrównany', 'niezwykla', 'niezwykły', 'niezywkła',
+            'oczarowana', 'oczarowany', 'oryginalnie', 'oryginalność', 'oryginalny', 'oszałamiająca', 'oszałamiające',
+            'oszałamiający', 'pełen', 'pełna', 'pochłonęła', 'podekscytowana', 'podekscytowanie', 'podekscytowany',
+            'pokochałam', 'pokochałem', 'polecam', 'pomysłowa', 'pomysłowy', 'porusza', 'poruszająca', 'poruszyła',
+            'potężny', 'pozytwna', 'pozytywnie', 'przygoda', 'przyjemna', 'przyjemne', 'przyjemnie', 'przyjemnie',
+            'przyjemny', 'radosna', 'radosne', 'radosne', 'radośnie', 'rozkosz', 'rozkoszna', 'rozkosznie', 'rozkoszny',
+            'rzetelna', 'rzetelnie', 'rzetelność', 'silna', 'silnie', 'silny', 'sukces', 'szczera', 'szczerość',
+            'szczery', 'szczerze', 'szlachetna', 'szlachetnie', 'szlachetny', 'sztuka', 'twórcza', 'twórcze', 'twórczy',
+            'udana', 'udanie', 'ukochana', 'ukochany', 'ulubiona', 'ulubione', 'urok', 'uwielbiam', 'uśmiech', 'warto',
+            'wartościowa', 'wartościowe', 'wciągająca', 'wciągnął', 'wciągnęła', 'wow', 'wyobraźnia', 'wyobraźnię',
+            'zabawa', 'zabawa', 'zabawna', 'zabawnie', 'zachwycona', 'zachwycony', 'zachwyt', 'zaciekawienie',
+            'zaciekawiona', 'zaciekawiony', 'zaciekawiła', 'zafascynowanie', 'zafascynowała', 'zaintrygowała', 'zywa',
+            'ładna', 'ładnie', 'ładny', 'łał', 'świadoma', 'świadomie', 'świadomy', 'świetna', 'świetna', 'świetne',
+            'świetnie', 'świetnie', 'świetny', 'świetnyciekawa', 'żywo', 'żywy', '❤', '🌟', '🔥', '😃', '😅', '😉']
 
-NEGATIVE = ["najgorsza", "najgorzej", "najgorszy", "słaba", "słaby", "słabo" "porażka", "zła", "nudna", "nudno",
-            "nudnie",
-            "najnudniejsza", "najnudniejszy", "nuda", "nieciekawa", "nieciekawy", "nieciekawe", "nieciekawie",
-            "niedobra", "niedobry",
-            "niedobrze", "mieszane", "żałuję", "niby", "spadek","okropna","okropnie","okropny","nierealistyczne","nierealistyczna","nierealistycznie" ]
+NEGATIVE = ['bez wyrazu', 'brak', 'brakuje', 'drażni', 'drażniła', 'drażniły', 'głupia', 'głupio', 'głupią', 'kicz',
+            'kiczowata', 'mało', 'mieszane', 'minus', 'minusem', 'męczyłam', 'męczyłem', 'męcząca', 'męczące',
+            'męczący', 'mękazbędna', 'najgorsza', 'najgorszy', 'najgorzej', 'najnudniejsza', 'najnudniejszy', 'niby',
+            'nie podoba', 'nie podobała', 'nie porwał', 'nie porwała', 'nie trafia', 'nie trafiła', 'niechętna',
+            'niechętnie', 'nieciekawa', 'nieciekawe', 'nieciekawie', 'nieciekawy', 'niedobra', 'niedobry', 'niedobrze',
+            'nierealistyczna', 'nierealistyczne', 'nierealistycznie', 'niespójna', 'niespójna', 'niespójne',
+            'niespójnie', 'niespójny', 'nijacy', 'nijak', 'nijaka', 'nijaki', 'nijako', 'nuda', 'nudna', 'nudnie',
+            'nudno', 'nużąca', 'nużące', 'nużący', 'odradzam', 'okropna', 'okropnie', 'okropny', 'ostrzegam',
+            'ostrzeżenie', 'powtarza', 'powtarzalna', 'powtarzalny', 'pretensjonalna', 'pretensjonalną', 'problem',
+            'przykro', 'płytka', 'płytki', 'płytkie', 'spadek', 'szajs', 'słaba', 'słabiutko', 'słaboporażka', 'słaby',
+            'trudno', 'z trudem', 'zła', 'żałuję', '☹']
+
 print(len(POSITIVE))
 print(len(NEGATIVE))
+
 train_examples = []
 
 with open("src\\train_data.txt", "r", encoding="utf-8") as train_data:
